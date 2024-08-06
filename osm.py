204bb6326c46dc716c0f4186dbd50076e616cd03
tp1050 <tp1050@gmail.com>
2024-08-07 00:58:52 +0100

    dool

--- osm.py
+++ osm.py
@@ -10,11 +10,11 @@ center_lat = 35.6892
 center_lon = 51.3890
 min_lat, min_lon = 35.7848, 51.2853
 max_lat, max_lon = 35.5848, 51.5853
-step_size = 10000
+step_size = 500
 
 beauty_phrases = ["doctor"," متخصص پزشک","مطب زیبایی", "پوست و مو"]
 url = "http://192.168.1.7:12345/api/interpreter"
-medical_Est = ["hospital", "clinic","کلینیک_زیبایی","health_care","دکتر","پزشک","مطب","حکیم" ,"midwife", "dentist", "doctors", "doctor", "beauty-shop", "health", "healthcare", "beutician", "physician", "medical", "healthcare=doctor", "healthcare=dentist", "healthcare=midwife"]
+medical_Est = [ "کلینیک_زیبایی","health_care","دکتر","پزشک","مطب","حکیم" ,"midwife", "doctors", "doctor", "shop=beauty", "health", "healthcare", "beutician", "healthcare=doctor", "healthcare=dentist", "healthcare=midwife"]
 def q_q(phrase, lat, lon, radius_meters=5000):
     query = f"""
     [out:json];
@@ -25,7 +25,7 @@ def q_q(phrase, lat, lon, radius_meters=5000):
     """
     return query
 
-def am_q(amenity,lat, lon, radius_meters=10000):
+def am_q(amenity,lat, lon, radius_meters=2500):
     query = f"""
     [out:json];
     (
@@ -63,20 +63,37 @@ def find_doctors_nearby(lat, lon, phrases=None, amenities=None, radius_meters=50
             print("error", resp, resp.url, str(e))
     return results
 
+# def search_tehran():
+#     total_steps = ((int(min_lat * 1e6) - int(max_lat * 1e6)) // step_size + 1) * ((int(max_lon * 1e6) - int(min_lon * 1e6)) // step_size + 1)
+#     current_step = 0
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:  # Create a thread pool
+#         futures = []
+#         for lat in range(int(min_lat * 1e6), int(max_lat * 1e6), -step_size):
+#             for lon in range(int(min_lon * 1e6), int(max_lon * 1e6), step_size):
+#                 ret=find_doctors_nearby(lat=lat / 1000000, lon=lon / 1000000,phrases=beauty_phrases)
+#               # ret=find_doctors_nearby(lat=lat / 1000000, lon=lon / 1000000,medical_amenities=medical_amenities)
+
+#                 current_step += 1
+#                 progress = (current_step / total_steps) * 100
+#                 os.system('cls' if os.name == 'nt' else 'clear')
+#                 print(f">> Searching Latitude={lat / 1000000}, Longitude={lon / 1000000} ")
+#                 print(f">> Progress: {progress:.2f}% completed")
 def search_tehran():
     total_steps = ((int(min_lat * 1e6) - int(max_lat * 1e6)) // step_size + 1) * ((int(max_lon * 1e6) - int(min_lon * 1e6)) // step_size + 1)
     current_step = 0
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:  # Create a thread pool
         futures = []
         for lat in range(int(min_lat * 1e6), int(max_lat * 1e6), -step_size):
             for lon in range(int(min_lon * 1e6), int(max_lon * 1e6), step_size):
-                ret=find_doctors_nearby(lat=lat / 1000000, lon=lon / 1000000,phrases=beauty_phrases)
-              # ret=find_doctors_nearby(lat=lat / 1000000, lon=lon / 1000000,medical_amenities=medical_amenities)
+                futures.append(executor.submit(find_doctors_nearby, lat=lat / 1000000, lon=lon / 1000000,phrases=beauty_phrases))
+                futures.append(executor.submit(find_doctors_nearby, lat=lat / 1000000, lon=lon / 1000000,amenities=medical_Est))
 
-                current_step += 1
-                progress = (current_step / total_steps) * 100
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print(f">> Searching Latitude={lat / 1000000}, Longitude={lon / 1000000} ")
-                print(f">> Progress: {progress:.2f}% completed")
 
+        for future in concurrent.futures.as_completed(futures):
+            # ... (optional: handle results or errors from each thread) ...
+            current_step += 1
+            progress = (current_step / total_steps) * 100
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f">> Progress: {progress:.2f}% completed\n {future}")
 search_tehran()
